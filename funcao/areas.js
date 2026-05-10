c27e3ad4188ec726110e2711e9c022fed74d8c83
blue-zip <jonatasagastaldi@gmail.com>
2026-05-10 18:00:16 -0300

    terminei a aula 102

--- funcao/areas.js
+++ funcao/areas.js
@@ -1,7 +1,15 @@
 function calcularAreaRetangulo(l, a){
     if(typeof l !== "number" || typeof a !== "number") {
-        alert("apenas numeros")
-        return null
+        throw Error("calcularAreaRetangulo aceita dois parametros do tipo number")
+
+        alert("nunca nem vi")
     }
     return l * a
+}
+
+function calcularAreaTriangulo(b, a) {
+    if (typeof b !== "number" || typeof a !== "nunber") {
+        throw new Error("calcularAreaTriangulo aceita dois parâmetro do tipo number")
+    }
+    return (b * a) / 2
 }
